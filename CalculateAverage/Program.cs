f60190193f81ccac3dff6f01506f80f59187c15a
MBurakT <mbtbrk11@outlook.com>
2024-01-04 22:01:34 +0300

    Refactor CalculateAverage

--- CalculateAverage/Program.cs
+++ CalculateAverage/Program.cs
@@ -10,38 +10,17 @@ class Program
         while (true)
         {
             if (!int.TryParse(CustomFunctions.GetInputFromUser("Please enter depth of fibonnaci"), out int input))
-                return;
-
-            if (input == 0) break;
-
-            if (input > 2)
             {
-                int fib0 = 0, fib1 = 1, counter;
-                float total = 1;
-
-                for (counter = 2; counter < input; counter++)
-                {
-                    int fib = fib1 + fib0;
-                    fib0 = fib1;
-                    fib1 = fib;
-                    total += fib;
-                }
-
-                Console.WriteLine((total / input).ToString("F2"));
+                Console.WriteLine($"Value is not numeric! input: {input}");
             }
-            else if (input == 2)
+            else if (input <= 0)
             {
-                Console.WriteLine("0.5");
-                return;
-            }
-            else if (input == 1)
-            {
-                Console.WriteLine("0");
+                Console.WriteLine($"Value is zero or negative! input: {input}");
+                break;
             }
             else
             {
-                Console.WriteLine("input is negative!");
-                return;
+                Console.WriteLine(CustomFunctions.Average(CustomFunctions.CreateFibonacciArray(input)).ToString("F2"));
             }
         }
     }
--- Utilities/Functions/CustomFunctions.cs
+++ Utilities/Functions/CustomFunctions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Utilities;
 
@@ -8,7 +9,7 @@ public class CustomFunctions
     {
         if (limit == 0)
         {
-            throw new Exception("Input is invalid, limit exceeded!");
+            throw new Exception("Input is invalid, maximum retry number exceeded!");
         }
 
         Console.Write($"{message}: ");
@@ -18,5 +19,39 @@ public class CustomFunctions
         if (input is null) input = GetInputFromUser(message, limit - 1);
 
         return input;
+    }
+
+    public static double Average(int[] array)
+    {
+        return (double)array.Sum() / array.Length;
+    }
+
+    public static int[] CreateFibonacciArray(int depth)
+    {
+        if (depth > 2)
+        {
+            int[] fib = new int[depth];
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int counter = 2; counter < depth; counter++)
+            {
+                fib[counter] = fib[counter - 1] + fib[counter - 2];
+            }
+
+            return fib;
+        }
+        else if (depth == 2)
+        {
+            return [0, 1];
+        }
+        else if (depth == 1)
+        {
+            return [0];
+        }
+        else
+        {
+            return [];
+        }
     }
 }
