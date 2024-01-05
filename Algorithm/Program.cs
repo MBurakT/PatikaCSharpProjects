891a4d73512ae2cc235e568556885c4c2e74223f
MBurakT <mbtbrk11@outlook.com>
2024-01-05 19:35:34 +0300

    Algorithm

---
+++ Algorithm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Utilities;
+
+namespace Algorithm;
+
+class Program
+{
+    static void Run(string[] args)
+    {
+        string[] inputs = CustomFunctions.GetInputFromUser("Input").Split(',');
+
+        Console.WriteLine(inputs[0].Remove(int.Parse(inputs[1]), 1));
+    }
+    static void Main(string[] args)
+    {
+        try
+        {
+            Run(args);
+            if (Console.ReadLine() == "clear") Console.Clear();
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+        }
+    }
+}
