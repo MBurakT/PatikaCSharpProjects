6b8da11d31cc1549670d7dc2f091d6ee13893896
MBurakT <mbtbrk11@outlook.com>
2024-01-05 19:45:22 +0300

    ReverseString

---
+++ ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Utilities;
+
+namespace Algorithm;
+
+class Program
+{
+    static void Run(string[] args)
+    {
+        string input = CustomFunctions.GetInputFromUser("Input");
+
+        Console.WriteLine(input.Reverse().ToArray());
+    }
+    static void Main(string[] args)
+    {
+        try
+        {
+            Run(args);
+            if (Console.ReadLine() == "clear") Console.Clear();
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+        }
+    }
+}
